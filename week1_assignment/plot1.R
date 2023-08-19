eebf0f990d3906d816ccab23c66c26dd245b0f06
jimy-14 <jamilabano1409@gmail.com>
2023-08-19 13:10:07 +0400

    first commit

---
+++ week1_assignment/plot1.R
@@ -0,0 +1,7 @@
+cols=c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3')
+df<- read.table('household_power_consumption.txt',sep=';',na.strings='?', header = TRUE,nrows=2880, skip=66636,col.names =cols,stringsAsFactors = FALSE)
+df$datetime<- paste(df$Date,df$Time)  
+df$datetime<-as.POSIXct(df$datetime,format = "%d/%m/%Y %H:%M:%S")
+hist(df$Global_active_power,col='red',xlab="Global active power(kilowatts)",main="Global active power")
+dev.copy(png,"plot1.png",height=480, width=480)
+dev.off()
---
+++ week1_assignment/plot2.R
@@ -0,0 +1,7 @@
+cols=c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3')
+df<- read.table('household_power_consumption.txt',sep=';',na.strings='?', header = TRUE,nrows=2880, skip=66636,col.names =cols,stringsAsFactors = FALSE)
+df$datetime<- paste(df$Date,df$Time)  
+df$datetime<-as.POSIXct(df$datetime,format = "%d/%m/%Y %H:%M:%S")
+plot(df$datetime,df$Global_active_power,type="l",xlab="",ylab="Global active power (kilowatts)")
+dev.copy(png,"plot2.png",height=480, width=480)
+dev.off()
